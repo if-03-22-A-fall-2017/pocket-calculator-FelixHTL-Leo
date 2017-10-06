5d2295d70889751d5eae9f7cad1e81fed459ecc2
Felix Bogengruber <felixbogi@gmail.com>
2017-10-06 11:48:47 +0200

    All

--- pocketCalculater.c
+++ pocketCalculater.c
@@ -1,7 +1,58 @@
 #include <stdio.h>
+#include <limits.h>
 #define AMOUNT_OF_CHOICES 5
 
+int check_overflow(double overflow, int choice, double operand1, double operand2){
+  switch(choice){
+    case 1:
+    if(operand1 + operand2 > overflow){
+      return 1;
+    }
+      break;
+    case 2:
+    if(operand1 - operand2 > overflow){
+      return 1;
+    }
+      break;
+    case 3:
+    if(operand1 * operand2 > overflow){
+      return 1;
+    }
+      break;
+    case 4:
+    if(operand1 / operand2 > overflow){
+      return 1;
+    }
+      break;
+  }
+  return 0;
+}
 
+int check_underflow(double underflow, int choice, double operand1, double operand2){
+  switch(choice){
+    case 1:
+    if(operand1 + operand2 < underflow){
+      return 1;
+    }
+      break;
+    case 2:
+    if(operand1 - operand2 < underflow){
+      return 1;
+    }
+      break;
+    case 3:
+    if(operand1 * operand2 < underflow){
+      return 1;
+    }
+      break;
+    case 4:
+    if(operand1 / operand2 < underflow){
+      return 1;
+    }
+      break;
+  }
+  return 0;
+}
 int check_If_Correct(int number){
   for (int i = 0; i < AMOUNT_OF_CHOICES; i++) {
     if(number == i){
@@ -13,7 +64,7 @@ int check_If_Correct(int number){
 
 void get_operands(double* operand1, double* operand2)
 {
-    printf("Erster Operande/Zweiter Operand: ");
+    printf("Erster Operande/ Zweiter Operand: ");
     scanf("%lf/%lf", operand1, operand2 );
 
 }
@@ -40,28 +91,65 @@ int users_choice(){
 
 
 int main(int argc, char *argv[]) {
-  int choice = users_choice();
-  double operand1, operand2;
-  get_operands(&operand1, &operand2);
-  if(choice == 0){
-    return 0;
-  }
-  else if (choice == 1) {
-    printf("Result of %lf + %lf = %lf\n", operand1, operand2, operand1 + operand2);
-  }
-  else if(choice == 2){
-    printf("Result of %lf - %lf = %lf\n", operand1, operand2, operand1 - operand2);
-  }
-  else if(choice == 3){
-    printf("Result of %lf * %lf = %lf\n", operand1, operand2, operand1 * operand2);
-  }
-  else if(choice == 4){
-    if(operand2 == 0){
-      printf("Division durch null!!!\n");
-      return 0;
+  int choice;
+  do{
+    choice = users_choice();
+    double operand1, operand2;
+    double overflow = INT_MAX;
+    double underflow = INT_MIN;
+    if (choice == 1) {
+      get_operands(&operand1, &operand2);
+      if(check_overflow(overflow, choice, operand1, operand2) == 1){
+        printf("Its an overflow\n");
+      }
+      else if(check_underflow(underflow, choice, operand1, operand2) == 1){
+        printf("Its an overflow\n");
+      }
+      else{
+        printf("Result of %2lf + %2lf = %2lf\n", operand1, operand2, operand1 + operand2);
+      }
     }
-    printf("Result of %lf / %lf = %lf\n", operand1, operand2, operand1 / operand2);
+    else if(choice == 2){
+      get_operands(&operand1, &operand2);
+      if(check_overflow(overflow, choice, operand1, operand2) == 1){
+        printf("Its an overflow\n");
+      }
+      else if(check_underflow(underflow, choice, operand1, operand2) == 1){
+        printf("Its an overflow\n");
+      }
+      else{
+      printf("Result of %2lf - %2lf = %2lf\n", operand1, operand2, operand1 - operand2);
+      }
+    }
+    else if(choice == 3){
+      get_operands(&operand1, &operand2);
+      if(check_overflow(overflow, choice, operand1, operand2) == 1){
+        printf("Its an overflow\n");
+      }
+      else if(check_underflow(underflow, choice, operand1, operand2) == 1){
+        printf("Its an overflow\n");
+      }
+      else{
+      printf("Result of %2lf * %2lf = %2lf\n", operand1, operand2, operand1 * operand2);
+      }
+    }
+    else if(choice == 4){
+      get_operands(&operand1, &operand2);
+      if(operand2 == 0){
+        printf("Division durch null!!!\n");
+      }
+      else if(check_overflow(overflow, choice, operand1, operand2) == 1){
+        printf("Its an overflow\n");
+      }
+      else if(check_underflow(underflow, choice, operand1, operand2) == 1){
+        printf("Its an overflow\n");
+      }
+      else{
+        printf("Result of %2lf / %2lf = %2lf\n", operand1, operand2, operand1 / operand2);
+      }
   }
+
+}while(choice != 0);
 
 
   return 0;
